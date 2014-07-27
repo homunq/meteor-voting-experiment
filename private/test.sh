0ec86e9047c6721f26e370e84608120a07ad1230
Jameson Quinn <jameson.quinn+github@gmail.com>
2014-07-27 14:33:51 -0400

    scripts for running on amt

---
+++ private/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -f ../../aws-mturk-clt-1.3.1/samples/__bettercount
+cp -r __bettercount ../../aws-mturk-clt-1.3.1/samples/
+cp ../../aws-mturk-clt-1.3.1/bin/mturk.properties.jq ../../aws-mturk-clt-1.3.1/bin/mturk.properties
+cd ../../aws-mturk-clt-1.3.1/samples/__bettercount
+./run.sh
