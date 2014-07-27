2bfc3b54c527f5efa4652171386c9f8ae4facc60
Jameson Quinn <jameson.quinn+github@gmail.com>
2014-07-27 17:45:49 -0400

    remove force-ssl

---
+++ private/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf ../../aws-mturk-clt-1.3.1/samples/__bettercount
+cp -r __bettercount ../../aws-mturk-clt-1.3.1/samples/
+cp ../../aws-mturk-clt-1.3.1/bin/mturk.properties.lwm ../../aws-mturk-clt-1.3.1/bin/mturk.properties
+cd ../../aws-mturk-clt-1.3.1/samples/__bettercount
+./run.sh
