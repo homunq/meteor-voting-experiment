f4203d8c64774d125e5ad2a599bdb0d149975b7c
Jameson Quinn <jameson.quinn+github@gmail.com>
2014-07-27 15:48:10 -0400

    minor static fixes

--- private/test.sh
+++ private/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -f ../../aws-mturk-clt-1.3.1/samples/__bettercount
+rm -rf ../../aws-mturk-clt-1.3.1/samples/__bettercount
 cp -r __bettercount ../../aws-mturk-clt-1.3.1/samples/
 cp ../../aws-mturk-clt-1.3.1/bin/mturk.properties.jq ../../aws-mturk-clt-1.3.1/bin/mturk.properties
 cd ../../aws-mturk-clt-1.3.1/samples/__bettercount
